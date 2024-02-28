ccd3811e7f0aa25fff4092445104614f28166d8f
Alejandro Alonso Sanchez <alexaas110@gmail.com>
2024-02-28 11:15:01 -0600

    Implemented enemy AI for path-finding

--- scenes/enemies/slime.gd
+++ scenes/enemies/slime.gd
@@ -2,18 +2,22 @@ extends CharacterBody2D
 
 var current_path: Array[Vector2i]
 @onready var tilemap = $"../Ground"
-@export var starting_position = Vector2i(0,0)
-var finish_position
+@export var starting_tile = Vector2i(0,0)#tilemap.starting_tile
+var creature_speed = 0.75
+ 
+var finising_tile
 func _ready():
 	$AnimatedSprite2D.play()
-	current_path = tilemap.astar.get_id_path(starting_position, finish_position).slice(1)
+	position = tilemap.map_to_local(starting_tile)+ tilemap.astar.offset
+	finising_tile = tilemap.finishing_tile
+	current_path = tilemap.astar.get_id_path(starting_tile, finising_tile).slice(1)
 
 func _physics_process(_delta):
 	if current_path.is_empty():
+		$AnimatedSprite2D.stop()
 		return
-		
 	var target_position = tilemap.map_to_local(current_path.front())
-	global_position = global_position.move_toward(target_position, 1)
+	global_position = global_position.move_toward(target_position, creature_speed)
 	
 	if global_position == target_position:
 		current_path.pop_front()
