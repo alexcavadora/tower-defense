a41c151849160c40e72d1e875c7cdc36d2b8e5db
Alejandro Alonso Sanchez <alexaas110@gmail.com>
2024-03-08 21:16:34 -0600

    Created enemy spawner prototype.

--- scenes/enemies/slime.gd
+++ scenes/enemies/slime.gd
@@ -2,22 +2,16 @@ extends CharacterBody2D
 
 var current_path: Array[Vector2i]
 @onready var tilemap = $"../Ground"
-@export var starting_tile = Vector2i(0,0) #tilemap.starting_tile
-var creature_speed = 0.75
- 
-var finising_tile
-func _ready():
-	$AnimatedSprite2D.play()
-	position = tilemap.map_to_local(starting_tile)
-	finising_tile = tilemap.finishing_tile
-	current_path = tilemap.astar.get_id_path(starting_tile, finising_tile).slice(1)
+@onready var movement_component = $MovementComponent
+@onready var sprite_component = $SpriteComponent
+var startIndex = 0
 
-func _physics_process(_delta):
-	if current_path.is_empty():
-		$AnimatedSprite2D.stop()
-		return
-	var target_position = tilemap.map_to_local(current_path.front())
-	global_position = global_position.move_toward(target_position, creature_speed)
-	
-	if global_position == target_position:
-		current_path.pop_front()
+func _ready():
+	movement_component.init(tilemap.starting_tile[startIndex], tilemap.finishing_tile, tilemap)
+	sprite_component.start_animation()
+	movement_component.connect("path_completed", _on_path_completed)
+
+
+func _on_path_completed():
+	sprite_component.stop_animation()
+	queue_free()
